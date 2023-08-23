7cbc4261b8dc6834c44f5fdb30090e9edeefa9c3
chrisstrike <97127337+chrisstrike@users.noreply.github.com>
2023-08-23 16:31:44 -0700

    ALB mod 1

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
@@ -42,7 +42,6 @@ resource "aws_instance" "blog" {
   }
 }
 
-
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
@@ -53,11 +52,7 @@ module "alb" {
 
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
-
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -86,6 +81,8 @@ module "alb" {
     Environment = "dev"
   }
 }
+
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
